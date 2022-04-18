c19238a276f24e660d58f2a790ec318b617d11fb
feigo808 <feigo.code@gmail.com>
2022-04-17 22:25:43 -1000

    Set up navigation

--- app/src/main/java/com/feiyatsu/foodapp/MainActivity.kt
+++ app/src/main/java/com/feiyatsu/foodapp/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.feiyatsu.foodapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.feiyatsu.foodapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
+
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.homeFragment,
+                R.id.favoritesFragment,
+                R.id.categoriesFragment
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        // Setting Navigation Controller with the BottomNavigationView
+        binding.btmNav.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() || navController.navigateUp()
     }
 }
